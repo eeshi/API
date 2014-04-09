9f1eeb6c1ba2cc74bed5237b1007a4d9ed9b3e6c
adrianObel <obeladrian@gmail.com>
2014-04-09 18:26:49 -0430

    Remove automatic inclusion of _id on toObject call

--- models/jobposts.js
+++ models/jobposts.js
@@ -1,11 +1,20 @@
 var Schema = require('mongoose').Schema;
 
 module.exports = JobPosts = new Schema({
-  sourceId: String,
+  sourceId: String, 
   jobVacant: String,
   description: String,
   pageTitle: String,
   companyName: String,
   companyUrl: String,
-  url: String 
-});
+  url: { 
+    type: String,
+    unique: true
+  }
+}, { id: false });
+
+if (!JobPosts.options.toObject) JobPosts.options.toObject = {};
+JobPosts.options.toObject.transform = function (doc, ret, options) {
+  // remove the _id of every document before returning the result
+  delete ret._id;
+}
